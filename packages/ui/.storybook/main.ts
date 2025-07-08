5b88a749a206b485d80a18fce6fb2266fdbc59c6
Josh Birdwell <josh.birdwell@life.church>
2025-07-08 10:35:03 -0500

    chore(release): configures changesets for releases
    Adds changesets configuration to automate versioning and publishing of packages.
    
    This includes installing the changesets CLI, creating the necessary configuration files, and adding a publish script to the package.json.

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,27 +1,24 @@
-import type { StorybookConfig } from '@storybook/react-webpack5';
+import type { StorybookConfig } from "@storybook/react-webpack5";
 
-import { join, dirname } from "path"
+import { join, dirname } from "path";
 
 /**
-* This function is used to resolve the absolute path of a package.
-* It is needed in projects that use Yarn PnP or are set up within a monorepo.
-*/
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
 function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')))
+  return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    getAbsolutePath("@storybook/addon-webpack5-compiler-swc"),
+    getAbsolutePath("@storybook/addon-docs"),
+    getAbsolutePath("@storybook/addon-onboarding"),
   ],
-  "addons": [
-    getAbsolutePath('@storybook/addon-webpack5-compiler-swc'),
-    getAbsolutePath('@storybook/addon-docs'),
-    getAbsolutePath('@storybook/addon-onboarding')
-  ],
-  "framework": {
-    "name": getAbsolutePath('@storybook/react-webpack5'),
-    "options": {}
-  }
+  framework: {
+    name: getAbsolutePath("@storybook/react-webpack5"),
+    options: {},
+  },
 };
 export default config;
