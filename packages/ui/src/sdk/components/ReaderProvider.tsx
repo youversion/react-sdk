803aad90f138845a1585f8b17507058139d20cb3
Josh Birdwell <josh.birdwell@life.church>
2025-07-08 15:47:40 -0500

    Fix(ui): updates import source
    Updates the import source for core components from `@repo/core` to `@youversion/bible-core`.
    
    BA-

--- packages/ui/src/sdk/components/ReaderProvider.tsx
+++ packages/ui/src/sdk/components/ReaderProvider.tsx
@@ -2,7 +2,7 @@
 
 import { PropsWithChildren, ReactNode } from 'react';
 import { ReaderContext } from "../context/ReaderContext";
-import { Book, Chapter, Verse, Version } from "@repo/core";
+import { Book, Chapter, Verse, Version } from "@youversion/bible-core";
 
 type ReaderProviderProps = {
   children: ReactNode;
